e23df4bff216b157528a400c5a516ff720287e9a
Ivan <ivan@DESKTOP-GKCVFD6>
2024-07-10 21:04:11 +0300

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,28 +1,26 @@
-from random import randint
 from brain_games.cli import welcome_user
+from brain_games.scripts.utils import random_number, wrong_answer
 
 
 def even_game():
     name = welcome_user()
-    i = 0
-    while i < 3:
-        random_number = randint(1, 100)
+    rounds = 0
+    while rounds < 3:
+        number = random_number()
         print("Answer 'yes' if number even otherwise answer 'no'.")
-        print(f"Qusetion: {random_number}")
+        print(f"Qusetion: {number}")
         answer = str(input("Your asnwer: ")).lower()
-        condition_1 = random_number % 2 == 0 and answer == "yes"
-        condition_2 = random_number % 2 != 0 and answer == "no"
-        if condition_1 or condition_2:
-            i += 1
+        if number % 2 == 0:
+            correct_answer = "yes"
+        else:
+            correct_answer = "no"
+        if answer == correct_answer:
+            rounds += 1
             print("Correct!")
-            if i == 3:
+            if rounds == 3:
                 print(f"Congratulations, {name}!")
                 return True
         else:
-            print("Wrong")
-            restart = str(input("Do you want to try again? ")).lower()
-            if restart == "yes":
-                i = 0
-            else:
-                break
+            wrong_answer(answer, correct_answer, name)
+            break
     return False
