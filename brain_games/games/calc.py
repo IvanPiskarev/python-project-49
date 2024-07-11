9abcd56678a86bea1c4ae346776f73a7b5b805a5
Ivan <ivan@DESKTOP-GKCVFD6>
2024-07-12 02:11:12 +0300

    update utils.py & calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,5 +1,30 @@
+from random import choice
 from brain_games.cli import welcome_user
-from brain_games.scripts.utils import calc, wrong_answer
+from brain_games.scripts.utils import random_number, wrong_answer
+
+
+def calc():
+    number_1 = random_number()
+    number_2 = random_number()
+    arithmetic_operations = ["+", "-", "*"]
+    random_operator = choice(arithmetic_operations)
+    if int(number_1) > int(number_2):
+        question = f"{number_1} {random_operator} {number_2}"
+        if random_operator == "+":
+            correct_answer = number_1 + number_2
+        elif random_operator == "-":
+            correct_answer = number_1 - number_2
+        else:
+            correct_answer = number_1 * number_2
+    else:
+        question = f"{number_2} {random_operator} {number_1}"
+        if random_operator == "+":
+            correct_answer = number_2 + number_1
+        elif random_operator == "-":
+            correct_answer = number_2 - number_1
+        else:
+            correct_answer = number_2 * number_1
+    return question, correct_answer
 
 
 def calc_game():
--- brain_games/scripts/utils.py
+++ brain_games/scripts/utils.py
@@ -1,55 +1,8 @@
-from random import randint, choice
+from random import randint
 
 
-def random_number():
-    return randint(1, 10)
-
-
-def calc():
-    number_1 = random_number()
-    number_2 = random_number()
-    arithmetic_operations = ["+", "-", "*"]
-    random_operator = choice(arithmetic_operations)
-    if int(number_1) > int(number_2):
-        question = f"{number_1} {random_operator} {number_2}"
-        if random_operator == "+":
-            correct_answer = number_1 + number_2
-        elif random_operator == "-":
-            correct_answer = number_1 - number_2
-        else:
-            correct_answer = number_1 * number_2
-    else:
-        question = f"{number_2} {random_operator} {number_1}"
-        if random_operator == "+":
-            correct_answer = number_2 + number_1
-        elif random_operator == "-":
-            correct_answer = number_2 - number_1
-        else:
-            correct_answer = number_2 * number_1
-    return question, correct_answer
-
-
-def progression():
-    number = random_number
-    random_step = randint(1, 5)
-    result = [number]
-    for i in range(7):
-        result.append(int(result[i]) + random_step)
-    return result
-
-
-def prime_number(number):
-    if number == 1:
-        return False
-    if number == 2:
-        return True
-    i = 2
-    while i < number:
-        if number % i == 0:
-            return False
-        else:
-            i += 1
-    return True
+def random_number(max_value=10):
+    return randint(1, max_value)
 
 
 def wrong_answer(answer, c_answer, name):
