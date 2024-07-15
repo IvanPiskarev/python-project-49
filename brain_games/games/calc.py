4a98f2ca56485071ca097921a894377f7334aca3
Ivan <ivan@DESKTOP-GKCVFD6>
2024-07-16 01:30:59 +0300

    update calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,9 +1,7 @@
 import random
-from brain_games.cli import welcome_user
-from brain_games.scripts.utils import wrong_answer
 
 
-def calc():
+def calc_game():
     first_number = random.randint(1, 10)
     second_number = random.randint(1, 10)
     arithmetic_operations = ["+", "-", "*"]
@@ -16,23 +14,3 @@ def calc():
     else:
         correct_answer = first_number * second_number
     return question, correct_answer
-
-
-def calc_game():
-    name = welcome_user()
-    print("What is the result of the expression?")
-    rounds = 0
-    while rounds < 3:
-        question, correct_answer = calc()
-        print(question)
-        answer = int(input("Your asnwer: "))
-        if answer == int(correct_answer):
-            rounds += 1
-            print("Correct!")
-            if rounds == 3:
-                print(f"Congratulations, {name}!")
-                return True
-        else:
-            wrong_answer(answer, correct_answer, name)
-            break
-    return False
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.games.calc import calc_game
+from .utils import play_game
 
 
 def main():
-    calc_game()
+    play_game(calc_game, "calc")
 
 
 if __name__ == '__main__':
