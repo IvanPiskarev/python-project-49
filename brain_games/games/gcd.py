f24f828e71a98708acbce5b83cb599c497c73a4b
Ivan <ivan@DESKTOP-GKCVFD6>
2024-07-16 01:31:46 +0300

    update gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,30 +1,10 @@
 import random
 import math
-from brain_games.cli import welcome_user
-from brain_games.scripts.utils import wrong_answer
-
-
-def gcd(first_number, second_number):
-    return math.gcd(first_number, second_number)
 
 
 def gcd_game():
-    name = welcome_user()
-    rounds = 0
-    while rounds < 3:
-        first_number = random.randint(2, 100)
-        second_number = random.randint(2, 100)
-        correct_answer = gcd(first_number, second_number)
-        print("Find the greatest common divisor of given numbers.")
-        print(f"Question: {first_number} {second_number}")
-        answer = int(input("Your asnwer: "))
-        if answer == int(correct_answer):
-            rounds += 1
-            print("Correct!")
-            if rounds == 3:
-                print(f"Congratulations, {name}!")
-                return True
-        else:
-            wrong_answer(answer, correct_answer, name)
-            break
-    return False
+    first_number = random.randint(2, 100)
+    second_number = random.randint(2, 100)
+    question = f"Question: {first_number} {second_number}"
+    correct_answer = math.gcd(first_number, second_number)
+    return question, correct_answer
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.games.gcd import gcd_game
+from .utils import play_game
 
 
 def main():
-    gcd_game()
+    play_game(gcd_game, "gcd")
 
 
 if __name__ == '__main__':
