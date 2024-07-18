7771b7b4b44bcf136f4edfdbd196b299fce2cd73
Ivan <ivan@DESKTOP-GKCVFD6>
2024-07-18 23:49:05 +0300

    update gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,9 +2,12 @@ import random
 import math
 
 
-def gcd_game():
-    first_number = random.randint(2, 100)
-    second_number = random.randint(2, 100)
-    question = f"Question: {first_number} {second_number}"
-    correct_answer = math.gcd(first_number, second_number)
+GAME_RULE = 'Find the greatest common divisor of given numbers.'
+
+
+def game():
+    num1 = random.randint(2, 100)
+    num2 = random.randint(2, 100)
+    question = f"Question: {num1} {num2}"
+    correct_answer = math.gcd(num1, num2)
     return question, correct_answer
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.gcd import gcd_game
-from .utils import play_game
+from brain_games.games import gcd
+from brain_games.engine import play_game
 
 
 def main():
-    play_game(gcd_game, "gcd")
+    play_game(gcd)
 
 
 if __name__ == '__main__':
