8d4f34176aa9c195fa6dd4d616f7a9e920fd6f8a
Ivan <ivan@DESKTOP-GKCVFD6>
2024-07-18 23:49:46 +0300

    update prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,22 +1,27 @@
 import random
 
 
-def prime_game():
-    random_number = random.randint(1, 100)
-    question = f"Question: {random_number}"
-    if random_number == 1:
-        correct_answer = "no"
-    elif random_number == 2:
-        correct_answer = "yes"
-    i = 2
-    count = 0
-    while i < random_number:
-        if random_number % i == 0:
-            count += 1
-            break
+GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(num):
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+    x = 2
+    while x < num:
+        if num % x == 0:
+            return False
         else:
-            i += 1
-    if count == 0:
+            x += 1
+    return True
+
+
+def game():
+    num = random.randint(1, 100)
+    question = f"Question: {num}"
+    if is_prime(num) is True:
         correct_answer = "yes"
     else:
         correct_answer = "no"
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.prime import prime_game
-from .utils import play_game
+from brain_games.games import prime
+from brain_games.engine import play_game
 
 
 def main():
-    play_game(prime_game, "prime")
+    play_game(prime)
 
 
 if __name__ == '__main__':
